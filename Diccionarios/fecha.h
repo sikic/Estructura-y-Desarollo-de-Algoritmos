ef6a93758dcde55305699338fdde80095caebe4d
luis rojas <luis_brabo@hotmail.com>
2019-05-26 19:31:47 +0200

    Create fecha.h

---
+++ Diccionarios/fecha.h
@@ -0,0 +1,50 @@
+#ifndef Fecha_h
+#define Fecha_h
+
+//blibliotecas
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+class fecha {
+private:
+	int dia,horas, minutos;
+public:
+	fecha() {};
+	fecha(int d, int h, int m) {
+		dia = d;
+		horas = h;
+		minutos = m;
+	}
+
+	int getDia() const {
+		return dia;
+	}
+
+	int getHora() const {
+		return horas;
+	}
+	int getMinutos() const {
+		return minutos;
+	}
+	bool operator< (fecha &f) const {
+		if (dia != f.dia)
+			return dia < f.dia;
+		else {
+			if (horas != f.horas)
+				return horas < f.horas;
+			else 
+				return minutos < f.minutos;
+				
+		}
+	}
+	
+};
+
+#endif // !Fecha_h
+
+
+
+
+
+
